c3aacffea1f0d971678a58ca502be40ef9c8f5ed
TarunVanshkar <tarunben33@gmail.com>
2023-02-19 21:06:25 +0530

    Response_DTO

--- src/main/java/com/example/Student_Library_Management_System/Controllers/AuthorController.java
+++ src/main/java/com/example/Student_Library_Management_System/Controllers/AuthorController.java
@@ -2,13 +2,11 @@ package com.example.Student_Library_Management_System.Controllers;
 
 
 import com.example.Student_Library_Management_System.DTOs.AuthorEntryDto;
+import com.example.Student_Library_Management_System.DTOs.AuthorResponseDto;
 import com.example.Student_Library_Management_System.Models.Author;
 import com.example.Student_Library_Management_System.Services.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("author")
@@ -31,5 +29,20 @@ public class AuthorController
     public String addAuthor(@RequestBody AuthorEntryDto authorEntryDto)
     {
         return authorService.createAuthor(authorEntryDto);
+    }
+
+
+//    @GetMapping("/getAuthor")
+//    public Author getAuthor(@RequestParam("authorId") int id)
+//    {
+//        return authorService.getAuthor(id);
+//    }
+    // The above API will stuck in infinite recursion due to a list of book which author is having
+    // Its solution is below
+
+    @GetMapping("/getAuthor")
+    public AuthorResponseDto AuthorResponseDto(@RequestParam("authorId") int id)
+    {
+        return authorService.AuthorResponseDto(id);
     }
 }
--- src/main/java/com/example/Student_Library_Management_System/Services/AuthorService.java
+++ src/main/java/com/example/Student_Library_Management_System/Services/AuthorService.java
@@ -2,10 +2,16 @@ package com.example.Student_Library_Management_System.Services;
 
 
 import com.example.Student_Library_Management_System.DTOs.AuthorEntryDto;
+import com.example.Student_Library_Management_System.DTOs.AuthorResponseDto;
+import com.example.Student_Library_Management_System.DTOs.BookResponseDto;
 import com.example.Student_Library_Management_System.Models.Author;
+import com.example.Student_Library_Management_System.Models.Book;
 import com.example.Student_Library_Management_System.Repositories.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class AuthorService
@@ -43,5 +49,45 @@ public class AuthorService
 
         authorRepository.save(author);   // Because Repository layer only interact with object , not with DTOs
         return "Author added successfully";
+    }
+
+//    public Author getAuthor(int authorId)
+//    {
+//        return authorRepository.findById(authorId).get();
+//    }
+    // The above API will stuck in infinite recursion due to a list of book which author is having
+    // To resolve it, we will return a response Dto by below Api
+
+    public AuthorResponseDto AuthorResponseDto(int authorId)
+    {
+        Author author = authorRepository.findById(authorId).get();
+
+        // Now create authorResponseDto
+        AuthorResponseDto authorResponseDto = new AuthorResponseDto();
+
+        // Now set its attributes
+        // Convert List<Book> ----> List<BookResponseDto>
+        List<Book> booksList = author.getBooksWritten();
+
+        List<BookResponseDto> bookWritten = new ArrayList<>();
+        for(Book b : booksList)
+        {
+            BookResponseDto bookResponseDto = new BookResponseDto();
+            // Set its attributes
+            bookResponseDto.setGenre(b.getGenre());
+            bookResponseDto.setPages(b.getPages());
+            bookResponseDto.setName(b.getName());
+
+            bookWritten.add(bookResponseDto);
+        }
+
+        // Now set attributes for AuthorResponseDto
+        authorResponseDto.setBooksWritten(bookWritten);
+        authorResponseDto.setName(author.getName());
+        authorResponseDto.setAge(author.getAge());
+        authorResponseDto.setRating(author.getRating());
+        authorResponseDto.setCountry(author.getCountry());
+        return authorResponseDto;
+
     }
 }
